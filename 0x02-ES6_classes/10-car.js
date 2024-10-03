1cd3906d270348190be55adb4bb33656a1ce2900
fred maicah <fredmaicah@gmail.com>
2024-10-03 15:18:10 +0000

    My 0x02. ES6 classes commit 2

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,13 +1,16 @@
-/* eslint-disable class-methods-use-this */
-import Car from './10-car';
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
 
-export default class EVCar extends Car {
-  constructor(brand, motor, color, range) {
-    super(brand, motor, color);
-    this._range = range;
+  static get [Symbol.species]() {
+    return this;
   }
 
   cloneCar() {
-    return new Car();
+    const Species = this.constructor[Symbol.species];
+    return new Species();
   }
 }
